1697b50c68b7b041c0452df9a9fc587216aebd72
Karine <karinevitoria.marinho@gmail.com>
2022-01-26 14:40:43 -0300

    end delete and save

--- http/public/script.js
+++ http/public/script.js
@@ -3,13 +3,20 @@ const input = document.querySelector("input")
 const form = document.querySelector('form')
 
 async function load() {
-    const res = await fetch("http://localhost:3000/")
-    .then((data) => data.json())
+    const res = await fetch("http://localhost:3000/").then((data) => data.json())
 
     res.urls.map(({ name, url }) => addElement({ name, url }))
 }
 
 load()
+
+async function save({name, url}) {
+    await fetch(`http://localhost:3000/?name=${name}&url=${url}`).then((data) => data.json())
+}
+
+async function del({name, url}) {
+    await fetch(`http://localhost:3000/?name=${name}&url=${url}&del=1`).then((data) => data.json())
+}
 
 function addElement({ name, url }) {
     const li = document.createElement('li')
@@ -21,16 +28,19 @@ function addElement({ name, url }) {
     a.target = "_blank"
 
     trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.onclick = () => removeElement(trash, name, url)
 
     li.append(a)
     li.append(trash)
     ul.append(li)
+
 }
 
-function removeElement(el) {
+function removeElement(el, name, url) {
     if (confirm('Tem certeza que deseja deletar?'))
         el.parentNode.remove()
+
+    del({name, url})
 }
 
 form.addEventListener("submit", (event) => {
@@ -50,6 +60,8 @@ form.addEventListener("submit", (event) => {
         return alert("Digite a url da maneira correta")
 
     addElement({ name, url })
+
+    save({name, url})
 
     input.value = ""
 })
